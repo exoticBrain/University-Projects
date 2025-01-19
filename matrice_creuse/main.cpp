837c69791fd9d8addbb4440b5705ee977645fd4b
exoticBrain <t.mojahdi@esisa.ac.ma>
2025-01-19 18:40:07 +0100

    Done the first part for the surface but now comes the work for matrix creation

--- matrice_creuse/main.cpp
+++ matrice_creuse/main.cpp
@@ -1,21 +1,27 @@
 #include "datatypes.h"
 
 int main() {
+
+    PMat M1, M2, M3, M4, S, Res;
   std::string line = file_manipulation();
+
+  // --- Splitting the numbers into tokens of chars
   char *matrix = strtok((char *)line.c_str(), " ");
 
+  // --- Getting the line and column numbers
   int line_number = atoi(matrix);
-  // take subsequent tokens
   matrix = strtok(NULL, " ");
 
-  int colonne_number = atoi(matrix);
-  // take subsequent tokens
+  int column_number = atoi(matrix);
   matrix = strtok(NULL, " ");
 
   std::cout << "line : " << line_number << std::endl;
-  std::cout << "colonne : " << colonne_number << std::endl;
+  std::cout << "colonne : " << column_number << std::endl;
   while (matrix) {
-
+    for (int i = 0; i < line_number; i++) {
+      for (int j = 0; j < column_number; j++) {
+      }
+    }
     // take subsequent tokens
     matrix = strtok(NULL, " ");
   }
